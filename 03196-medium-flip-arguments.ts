81179954f1506326774f25544b86d77126d6a851
wangfengyuan <644301374@qq.com>
2022-11-14 22:12:49 +0800

    feat: flip args

---
+++ 03196-medium-flip-arguments.ts
@@ -0,0 +1,24 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
+  Expect<Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>>,
+  Expect<Equal<FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>, (arg0: boolean, arg1: number, arg2: string) => void>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  FlipArguments<'string'>,
+  // @ts-expect-error
+  FlipArguments<{ key: 'value' }>,
+  // @ts-expect-error
+  FlipArguments<['apple', 'banana', 100, { a: 1 }]>,
+  // @ts-expect-error
+  FlipArguments<null | undefined>,
+]
+
+
+// ============= Your Code Here =============
+type Reverse<T extends any[]> = T extends [infer F, ...infer R] ? [...Reverse<R>, F] : []
+type FlipArguments<T extends (...arg: any[]) => any> = T extends (...arg: infer P) => infer R ? (...arg: Reverse<P>) => R : never
